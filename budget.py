85fa4951293c1d73d9da571ff1f7eecc4ee83e58
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2023-11-28 17:24:22 -0300

    Commit of the first three python projects from FreeCodeCamp

---
+++ budget.py
@@ -0,0 +1,12 @@
+'''
+third exercise from FreeCodeCamp https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/budget-app
+'''
+class Category:
+    def __init__(self, category):
+        self.category = category
+        self.ledger = []
+
+
+
+teste = Category("Comida")
+print (teste.category)
