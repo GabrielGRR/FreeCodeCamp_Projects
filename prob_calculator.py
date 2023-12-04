f36ac925bbb10fb321825f831510b5cb9efe162e
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2023-12-04 17:42:53 -0300

    att

--- prob_calculator.py
+++ prob_calculator.py
@@ -2,17 +2,24 @@
 
 
 class Hat:
-    def __init__(self, **colors): #kwargs
-        for key, value in colors.items():
-            print(f"- {key}: {value}")
+    def __init__(self, **colors):
+        self.contents = []  # Inicializa a variável contents como uma lista vazia
+
+        # Adiciona cada cor à lista contents conforme a quantidade especificada
+        for color, quantity in colors.items():  # items() retorna uma lista de tuplas (chave, valor)
+            self.contents.extend([color] * quantity) # extend() adiciona cada elemento da lista colors à lista contents
+            #exemplo de output: ["red", "red", "blue"]
+
 
 
 hat1 = Hat(yellow=3, blue=2, green=6)
 hat2 = Hat(red=5, orange=4)
 hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
 
-#probabilidade = n de casos favoráveis / n de casos possiveis
+print(hat1.contents)
 
+#probabilidade = n de casos favoráveis / n de casos possiveis
+""" 
 print(5/2)
 print(5//2)
-print(5%2)
+print(5%2) """
