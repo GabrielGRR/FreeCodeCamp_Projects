adc3c32d68517e3af4b53e7eb9a25e18c51f860f
GabrielGRR <gabrielguimaraesr.rocha@gmail.com>
2023-12-13 21:03:57 -0300

    creating create_spend_chart function

--- budget.py
+++ budget.py
@@ -2,16 +2,18 @@
 third exercise from FreeCodeCamp https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/budget-app
 '''
 class Category:
-
+    #instances list
+    instances = []
+    
     #FUNCTION CONSTRUCTOR: ["Category", dict.items()*n]
     #EXPECTED OUTPUT EXAMPLE: 
     #['Food', {'amount': 200, 'description': 'Hamburguer'}, {'amount': 25, 'description': 'Transfer from Clothes'}]
-    
+
     def __init__(self, category):
         self.ledger = []
         self.category = category
         self.ledger.extend([category])
-
+        self.instances.append(category) #adiciona lista de instancias
     #RECEIPT FUNCTION, EXPECTED OUTPUT EXAMPLE:
     # *************Food*************
     # initial deposit         200.00
@@ -68,6 +70,35 @@ class Category:
         else:
             return True
 
+def create_spend_chart():
+    category_text = "Percentage spent by category\n"
+    test_maximum_value = 500
+    category_value = 350 #self.get_balance()
+    rounded_percentage = round(category_value/test_maximum_value*10)*10 #arredonda para o valor mais próximo multiplo de 10
+    percentage = 100
+    qtt_categories = len(Category.instances)
+    while percentage >= 0:
+        category_text += f"{percentage:>3}"+"|"
+        if rounded_percentage >= percentage:
+            category_text += "  o"
+        category_text += "\n"
+        percentage -= 10
+    category_text+="    "+"-"*4
+    return category_text
+
+"""     first_deposit = self.ledger[1]['amount']
+    category_text += f"{'initial deposit':<23}" + f'{first_deposit:>7.2f}\n'
+    receipt_item = ""
+    for item in self.ledger[1:]:
+        receipt_item = f"{item['description']:<23}" + f"{item['amount']:>7.2f}\n"
+        category_text += receipt_item
+    category_text += f"Total: {self.get_balance():.2f}"
+    return category_text """
+
+
+print(round(320/500*10)*10)
+
+
 food = Category("Food")
 clothes = Category("Clothes")
 food.deposit(200, "Hamburguer")
@@ -76,8 +107,9 @@ clothes.withdraw(-50, "Shirt")
 clothes.transfer(25, food)
 print(food)
 print(food.ledger)
-
-
+#create_spend_chart(food)
+print(create_spend_chart())
+print((Category.instances))
 
 """ food = Category("Food")
 clothes = Category("Clothes")
